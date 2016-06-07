8f7a1062331937391ad4001ab8358d881d9b31a6
Bryan Andrews <bryanandrews@gmail.com>
2016-06-07 15:18:48 -0700

    qa script updated to exist with a proper error level on error

--- qa.sh
+++ qa.sh
@@ -5,15 +5,16 @@
 
 echo "Running lint test..."
 bundle exec rake lint 2>/dev/null
-test $? -ne 0 && exit;
+test $? -ne 0 && exit $?;
 
 echo "Running syntax test..."
 bundle exec rake syntax 2>/dev/null
-test $? -ne 0 && exit;
+test $? -ne 0 && exit $?;
 
 echo "Running PF metadata_lint test..."
 bundle exec rake metadata_lint 2>/dev/null
-test $? -ne 0 && exit;
+test $? -ne 0 && exit $?;
 
 echo "Running rspec unit tests..."
 bundle exec rake spec 2>/dev/null
+test $? -ne 0 && exit $?;
